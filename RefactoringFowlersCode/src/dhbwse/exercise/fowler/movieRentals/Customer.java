91c2bef3f267a3f4c79a280193179244c40c8068
jasonmf123 <jari94@gmx.de>
2019-05-02 11:13:40 +0200

    Replace temp totalAmount and FRP with Queries and extract Queries to Methods

--- RefactoringFowlersCode/src/dhbwse/exercise/fowler/movieRentals/Customer.java
+++ RefactoringFowlersCode/src/dhbwse/exercise/fowler/movieRentals/Customer.java
@@ -2,7 +2,7 @@ package dhbwse.exercise.fowler.movieRentals;
 
 import java.util.ArrayList;
 
-class Customer {
+public class Customer {
 	private String name;
 	private ArrayList<Rental> rentals = new ArrayList<Rental>();
 
@@ -19,25 +19,35 @@ class Customer {
 	};
 
 	public String createBill() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
+		// add header
 		String result = "Rental Record for " + this.getName() + "\n";
 		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
 		for (Rental rental : rentals) {
-			
-			frequentRenterPoints += rental.calculateFrequentRenterPointsFor();
 			// show figures for this rental
 			result += rental.printBillingLineForRental();
-			totalAmount += rental.calculateBillingAmount();;
 		}
+		
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "Amount owed is " + String.valueOf(calculateTotalBillingAmount()) + "\n";
+		result += "You earned " + String.valueOf(calculateTotalFrequentRenterPoints()) + " frequent renter points";
 		return result;
 	}
 
+	private int calculateTotalFrequentRenterPoints() {
+		int frequentRenterPoints = 0;
+		for (Rental rental : rentals) {
+			frequentRenterPoints += rental.calculateFrequentRenterPointsFor();
+		}
+		return frequentRenterPoints;
+	}
 
-
+	private double calculateTotalBillingAmount() {
+		double totalAmount = 0;
+		for (Rental rental : rentals) {
+			totalAmount += rental.calculateBillingAmount();
+		}
+		return totalAmount;
+	}
 
 }
